120fcbc48d07db9336383c39531bc1fb19a9c2f3
raulbruno1 <brunoraulok@gmail.com>
2026-01-03 18:47:41 -0300

    cambios en public-home

--- src/app/components/public-home/public-home.component.ts
+++ src/app/components/public-home/public-home.component.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-public-home',
-  imports: [],
+  standalone: true,
   templateUrl: './public-home.component.html',
-  styleUrl: './public-home.component.css'
+  styleUrls: ['./public-home.component.css']
 })
 export class PublicHome {
-  constructor(
-    private router: Router
-  ) {}
 
-  navigate(): void{
-    this.router.navigate(['/login'])
+  constructor(private router: Router) {}
+
+  navigate(path: 'login' | 'register'): void {
+    this.router.navigate([path]);
   }
 }
