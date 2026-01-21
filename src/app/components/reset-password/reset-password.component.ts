e9fafefe39d20808b659c70cfaab0fa3001884e2
Feliciano <felicianomozzi02.utn@gmail.com>
2026-01-21 14:11:37 -0300

    Agregado que cuando está en la pantalla 'Reset-Password' y apreta BackToLogin el sistema setee la flag 'IsResettingPassword' otra vez a false

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -155,6 +155,8 @@ export class ResetPasswordComponent {
   }
 
   goToLogin(): void {
+    this.isLoading = false;
+    this.authService.setIsResettingPassword(false);
     this.router.navigate(['/login']);
   }
 }
