f9c0b0532d3a1fa810fe8ef04381c3c40695b4aa
Feliciano <felicianomozzi02.utn@gmail.com>
2026-02-24 10:45:05 -0300

    Unificada la visual global de la app, salvo por los componentes Auth y el public-home. Se movieron clases generales a styles.css, se agregaorn variables y luego se acomodaron todos los HTML y CSS de los componentes para que vayan acorde a estas nuevas configuraciones

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from "@angular/router";
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -15,5 +16,7 @@ export class HomeComponent {
     this.router.navigate([path]);
   }
 
+  // (optional mock)
+  userType: 'STUDENT' | 'TEACHER' | 'COORDINATOR' = 'TEACHER';
 }
 
