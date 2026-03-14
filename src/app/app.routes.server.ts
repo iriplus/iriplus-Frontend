4c2073e99e8293edf6e06f916b4aaa2c2c143d41
Feliciano <felicianomozzi02.utn@gmail.com>
2026-03-14 18:05:45 -0300

    Cambiado el budget inicial para el build y también se cambió la forma de renderizar las rutas con parametros en app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'exam-review/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'exam-revise/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'exam-resolve/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'view-exam/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
