05b83a60c50a6f732c12057154e691938bade99f
Feliciano <felicianomozzi02.utn@gmail.com>
2026-03-15 17:34:49 -0300

    Implementado la Home de Student, reutilizando componente, servicio y controlador Analytics

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -2,16 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { HomeAnalyticsResponse, CoordinatorAnalyticsStats } from '../interfaces/analytics.interface';
+import { HomeAnalyticsResponse } from '../interfaces/analytics.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnalyticsService {
-  /**
-   * Home analytics endpoint.
-   * The backend decides which role-specific block can be safely returned.
-   */
   private readonly HOME_URL = `${environment.backendUrl}/home`;
 
   constructor(private readonly http: HttpClient) {}
