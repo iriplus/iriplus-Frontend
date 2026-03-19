541cdcab47ff0dc7ec2ce578fc5c5bc57c2ed205
Feliciano <felicianomozzi02.utn@gmail.com>
2026-03-19 17:49:19 -0300

    Agregado los user manuals al footer dependiendo del tipo de usuario. La URL está hardcodeada en el HTML y va a nuestro google drive

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserType } from '../../interfaces/user.interface';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-footer',
+  imports: [ CommonModule ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  userType: UserType | null = null;
 
+  constructor(
+    private authService: AuthService,
+  ) {}
+
+  ngOnInit(): void {
+    this.loadUser();
+  }
+
+  loadUser(): void {
+    this.authService.loadMe().subscribe({
+      next: (user) => {
+        if (!user) {
+          return;
+        }
+        this.userType = user.type;
+      },
+      error: (err) => {
+        console.error('Error loading current user:', err);
+      },
+    });
+  }
+
+  get isStudent(): boolean {
+      return this.userType === UserType.STUDENT;
+  }
+  
+  get isTeacher(): boolean {
+      return this.userType === UserType.TEACHER;
+  }
+  
+  get isCoordinator(): boolean {
+      return this.userType === UserType.COORDINATOR;
+  }
 }
